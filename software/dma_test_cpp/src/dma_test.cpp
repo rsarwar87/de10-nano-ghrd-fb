cdca035a66cb5491d61dfb59b46ecd0f73f43ca1
Rashed Sarwar <rashed.sarwar@ukaea.uk>
2018-10-27 19:14:49 +0100

    packet wise

--- software/dma_test_cpp/src/dma_test.cpp
+++ software/dma_test_cpp/src/dma_test.cpp
@@ -21,10 +21,10 @@
 
 #define R2S_mSGDMA_CSR_BASEADDR 0xFF2a1000
 #define R2S_mSGDMA_DESC_BASEADDR 0xFF2a2000
-#define HPS_MEM_mSGDMA_SENADDR 0x36000000
+#define HPS_MEM_mSGDMA_SENADDR 0x3A000000
 #define S2R_mSGDMA_CSR_BASEADDR 0xFF2a3000
 #define S2R_mSGDMA_DESC_BASEADDR 0xFF2a4000
-#define HPS_MEM_mSGDMA_RECADDR 0x37000000
+#define HPS_MEM_mSGDMA_RECADDR 0x3B000000
 
 #define S2R_DISPATCHER_CSR_BASEADDR 0xFF2a1000
 #define S2R_DISPATCHER_DESC_BASEADDR 0xFF2a2000
@@ -47,9 +47,10 @@ int TestDispatcher()
 		// Set the HPS Memory start address
 		descriptor.write_addr = HPS_MEM_STORE_STARTADDR;
 		// We are using packetized streams so set length to max
-		descriptor.length = 0x20*3;
+		descriptor.length = 0xFFFFFFF;
 		// The dispatcher will use the End of Packet flag to end the transfer
-		descriptor.control.msBits.end_on_eop = 0;
+		descriptor.control.msBits.end_on_eop = 1;
+		//descriptor.control.msBits.park_wr = 1;
 		// Tell the dispatcher to start
 		descriptor.control.msBits.go = 1;
 
@@ -82,22 +83,24 @@ int TestBridge()
 		tsSGDMADescriptor descriptor_rs = {0};
 		// Set the HPS Memory start address
 		descriptor_rs.read_addr = HPS_MEM_mSGDMA_SENADDR;
-		descriptor_rs.write_addr = 0;
+		//descriptor_rs.write_addr = 0;
 		// We are using packetized streams so set length to max
 		descriptor_rs.length = 0x20*3;
 		// The dispatcher will use the End of Packet flag to end the transfer
-		descriptor_rs.control.msBits.end_on_eop = 0;
+		descriptor_rs.control.msBits.gen_sop = 1;
+		descriptor_rs.control.msBits.gen_eop = 1;
 		// Tell the dispatcher to start
 		descriptor_rs.control.msBits.go = 1;
 
 		// Create recv descriptor
 		tsSGDMADescriptor descriptor_sr = {0};
 		// Set the HPS Memory start address
-		descriptor_sr.read_addr = 0;
+		//descriptor_sr.read_addr = 0;
 		descriptor_sr.write_addr = HPS_MEM_mSGDMA_RECADDR;
 		// We are using packetized streams so set length to max
 		descriptor_sr.length = 0x20*3;
 		// The dispatcher will use the End of Packet flag to end the transfer
+
 		descriptor_sr.control.msBits.end_on_eop = 0;
 		// Tell the dispatcher to start
 		descriptor_sr.control.msBits.go = 1;
@@ -106,6 +109,7 @@ int TestBridge()
 		printf("Starting Descriptor, write starts from %x addr to %x addr\n\t\t ",
 								HPS_MEM_mSGDMA_SENADDR, HPS_MEM_mSGDMA_RECADDR);
 		ram2stream.WriteDescriptor(descriptor_rs);
+
 		stream2ram.WriteDescriptor(descriptor_sr);
 		tuSgdmaStatus ret1 = ram2stream.GetStatusReg();
 		tuSgdmaStatus ret2 = stream2ram.GetStatusReg();
