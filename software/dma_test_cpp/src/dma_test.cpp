d60094e7075db3a465a641504f0190befba2ca2e
Rashed Sarwar <rashed.sarwar@ukaea.uk>
2018-10-30 18:50:55 +0000

    added signal tap, dma_3 working

--- software/dma_test_cpp/src/dma_test.cpp
+++ software/dma_test_cpp/src/dma_test.cpp
@@ -19,21 +19,21 @@
 #define DISPATCHER_RESP_BASEADDR 0xFF2b3000
 #define HPS_MEM_STORE_STARTADDR 0x39000000
 
-#define R2S_mSGDMA_CSR_BASEADDR 0xFF2a1000
-#define R2S_mSGDMA_DESC_BASEADDR 0xFF2a2000
+#define R2S_mSGDMA_CSR_BASEADDR 0xFF2a5000
+#define R2S_mSGDMA_DESC_BASEADDR 0xFF2a6000
 #define HPS_MEM_mSGDMA_SENADDR 0x3A000000
-#define S2R_mSGDMA_CSR_BASEADDR 0xFF2a3000
-#define S2R_mSGDMA_DESC_BASEADDR 0xFF2a4000
+#define S2R_mSGDMA_CSR_BASEADDR 0xFF2a7000
+#define S2R_mSGDMA_DESC_BASEADDR 0xFF2a8000
 #define HPS_MEM_mSGDMA_RECADDR 0x3B000000
 
 #define S2R_DISPATCHER_CSR_BASEADDR 0xFF2a1000
 #define S2R_DISPATCHER_DESC_BASEADDR 0xFF2a2000
 #define S2R_DISPATCHER_RESP_BASEADDR 0xFF2a6000
-#define HPS_MEM_DISP_RECADDR 0x3A000000
+#define HPS_MEM_DISP_S2RDDR 0x3C000000
 #define R2S_DISPATCHER_CSR_BASEADDR 0xFF2a3000
 #define R2S_DISPATCHER_DESC_BASEADDR 0xFF2a4000
 #define R2S_DISPATCHER_RESP_BASEADDR 0xFF2a6000
-#define HPS_MEM_DISP_RECADDR 0x3B000000
+#define HPS_MEM_DISP_R2SADDR 0x3D000000
 
 
 int TestDispatcher()
@@ -62,7 +62,7 @@ int TestDispatcher()
 }
 
 
-int TestBridge()
+int TestBridgeDMAStd()
 {
 /*
 #define R2S_mSGDMA_CSR_BASEADDR 0xFF2a1000
@@ -73,10 +73,10 @@ int TestBridge()
 #define HPS_MEM_mSGDMA_RECADDR 0x37000000
 */
 		printf("\n\nCreating bridge control --  : ");
-		tcSGDMADispatcher ram2stream(R2S_DISPATCHER_CSR_BASEADDR,
-				R2S_DISPATCHER_DESC_BASEADDR , 0);
-		tcSGDMADispatcher stream2ram(S2R_DISPATCHER_CSR_BASEADDR,
-				S2R_DISPATCHER_DESC_BASEADDR, 0);
+		tcSGDMADispatcher ram2stream(R2S_mSGDMA_CSR_BASEADDR,
+				R2S_mSGDMA_DESC_BASEADDR , 0);
+		tcSGDMADispatcher stream2ram(S2R_mSGDMA_CSR_BASEADDR,
+				S2R_mSGDMA_DESC_BASEADDR, 0);
 		printf("DONE\n");
 
 		// Create send descriptor
@@ -108,9 +108,64 @@ int TestBridge()
 
 		printf("Starting Descriptor, write starts from %x addr to %x addr\n\t\t ",
 								HPS_MEM_mSGDMA_SENADDR, HPS_MEM_mSGDMA_RECADDR);
+		stream2ram.WriteDescriptor(descriptor_sr);
+		sleep(0.5);
 		ram2stream.WriteDescriptor(descriptor_rs);
 
+		tuSgdmaStatus ret1 = ram2stream.GetStatusReg();
+		tuSgdmaStatus ret2 = stream2ram.GetStatusReg();
+		printf("DONE r2s status=%x s2r status=%x\n", ret1.msBits, ret2.msBits);
+}
+int TestBridge()
+{
+/*
+#define R2S_mSGDMA_CSR_BASEADDR 0xFF2a1000
+#define R2S_mSGDMA_DESC_BASEADDR 0xFF2a2000
+#define HPS_MEM_mSGDMA_SENADDR 0x36000000
+#define S2R_mSGDMA_CSR_BASEADDR 0xFF2a3000
+#define S2R_mSGDMA_DESC_BASEADDR 0xFF2a4000
+#define HPS_MEM_mSGDMA_RECADDR 0x37000000
+*/
+		printf("\n\nCreating bridge control --  : ");
+		tcSGDMADispatcher ram2stream(S2R_DISPATCHER_CSR_BASEADDR,
+				R2S_DISPATCHER_DESC_BASEADDR , 0);
+		tcSGDMADispatcher stream2ram(S2R_DISPATCHER_CSR_BASEADDR,
+				S2R_DISPATCHER_DESC_BASEADDR, 0);
+		printf("DONE\n");
+
+		// Create send descriptor
+		tsSGDMADescriptor descriptor_rs = {0};
+		// Set the HPS Memory start address
+		descriptor_rs.read_addr = HPS_MEM_DISP_S2RDDR;
+		//descriptor_rs.write_addr = 0;
+		// We are using packetized streams so set length to max
+		descriptor_rs.length = 0x20*3;
+		// The dispatcher will use the End of Packet flag to end the transfer
+		descriptor_rs.control.msBits.gen_sop = 1;
+		descriptor_rs.control.msBits.gen_eop = 1;
+		// Tell the dispatcher to start
+		descriptor_rs.control.msBits.go = 1;
+
+		// Create recv descriptor
+		tsSGDMADescriptor descriptor_sr = {0};
+		// Set the HPS Memory start address
+		//descriptor_sr.read_addr = 0;
+		descriptor_sr.write_addr = HPS_MEM_DISP_R2SADDR;
+		// We are using packetized streams so set length to max
+		descriptor_sr.length = 0x20*3;
+		// The dispatcher will use the End of Packet flag to end the transfer
+
+		descriptor_sr.control.msBits.end_on_eop = 0;
+		// Tell the dispatcher to start
+		descriptor_sr.control.msBits.go = 1;
+
+
+		printf("Starting Descriptor, write starts from %x addr to %x addr\n\t\t ",
+								HPS_MEM_mSGDMA_SENADDR, HPS_MEM_mSGDMA_RECADDR);
 		stream2ram.WriteDescriptor(descriptor_sr);
+		sleep(0.5);
+		ram2stream.WriteDescriptor(descriptor_rs);
+
 		tuSgdmaStatus ret1 = ram2stream.GetStatusReg();
 		tuSgdmaStatus ret2 = stream2ram.GetStatusReg();
 		printf("DONE r2s status=%x s2r status=%x\n", ret1.msBits, ret2.msBits);
@@ -119,6 +174,7 @@ int TestBridge()
 int main(int argc, char* argv[])
 {
 	TestDispatcher();
+	TestBridgeDMAStd();
 	TestBridge();
 	return 0;
 }
